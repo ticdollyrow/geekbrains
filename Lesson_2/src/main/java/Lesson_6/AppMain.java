b90763e990d2c837d14bfe7a40a33423e59da78a
ticdollyrow <frauaja@yandex.ru>
2021-08-22 18:21:54 +0300

    Lesson_6

--- Lesson_2/src/main/java/Lesson_6/AppMain.java
+++ Lesson_2/src/main/java/Lesson_6/AppMain.java
@@ -1,2 +1,20 @@
-package Lesson_6;public class AppMain {
+package Lesson_6;
+
+public class AppMain {
+    public static void main(String[] args) {
+        Dog bobik = new Dog("Bobik");
+        bobik.run(600);
+        bobik.swim(7);
+
+        Dog elli = new Dog("Elli");
+        elli.run(100);
+
+        Cat poppy = new Cat("Poppy");
+        poppy.swim(3);
+        poppy.run(10);
+
+        Animal.printCount();
+        Dog.printCount();
+        Cat.printCount();
+    }
 }
