e79dbfcdc79594f1d78e13dce8a3e75e68dba1b6
Kate <frauaja@yandex.ru>
2021-10-11 22:45:12 +0300

    lesson3

--- GB_Algorithms&DataStructure/src/gb/algorithms/MainTest2.java
+++ GB_Algorithms&DataStructure/src/gb/algorithms/MainTest2.java
@@ -4,7 +4,7 @@ import gb.algorithms.search.MissingNumber;
 
 public class MainTest2 {
     public static void main(String[] args) {
-        int[] arr = {2};
+        int[] arr = {1,2,3,5,6,7,8};
         System.out.println(MissingNumber.search(arr,1));
     }
 }
--- GB_Algorithms&DataStructure/src/gb/algorithms/search/MissingNumber.java
+++ GB_Algorithms&DataStructure/src/gb/algorithms/search/MissingNumber.java
@@ -3,20 +3,21 @@ package gb.algorithms.search;
 import java.util.Arrays;
 
 public class MissingNumber {
-
+    private final static int FIRST_VALUE = 1;
 //    Дан массив из n элементов, начиная с 1. Каждый следующий элемент равен (предыдущий + 1).
 //    Но в массиве гарантированно 1 число пропущено. Необходимо вывести на экран пропущенное число.
 
     public static int search(int arr[], int step){
         int middle, value;
 
-        if( arr.length == 0 ) return step;
+        if(arr.length == 0) return FIRST_VALUE;
+        if(arr[0] != FIRST_VALUE) return  FIRST_VALUE;
         int low = 0;
         int high = arr.length;
 
-        while (low<high) {
+        while (low < high) {
             middle = (low + high) / 2;
-            value = ( middle + 1 ) * step;
+            value = middle  * step + FIRST_VALUE;
 
             if (arr[middle] == value) low = middle + 1;
             else high = middle;
