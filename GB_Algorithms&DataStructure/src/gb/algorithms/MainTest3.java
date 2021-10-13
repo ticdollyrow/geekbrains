39920221a19bfc9a5655ca46df66f8001a4b13f7
Kate <frauaja@yandex.ru>
2021-10-13 16:08:04 +0300

    lesson3

--- GB_Algorithms&DataStructure/src/gb/algorithms/MainTest3.java
+++ GB_Algorithms&DataStructure/src/gb/algorithms/MainTest3.java
@@ -1,4 +1,48 @@
 package gb.algorithms;
 
+import gb.algorithms.collections.deque.Deque;
+import gb.algorithms.collections.deque.DequeImpl;
+import gb.algorithms.collections.queue.Queue;
+import gb.algorithms.collections.queue.QueueImpl;
+
 public class MainTest3 {
+    public static void main(String[] args) {
+//        testQueue();
+        testDeue();
+    }
+
+    private static void testQueue() {
+
+        Queue<Integer> queue = new QueueImpl<>(5);
+        System.out.println("add element: " + queue.insert(34));
+        System.out.println("add element: " + queue.insert(12));
+        System.out.println("add element: " + queue.insert(20));
+        System.out.println("add element: " + queue.insert(16));
+        System.out.println("add element: " + queue.insert(14));
+        System.out.println("add element: " + queue.insert(17));
+
+        queue.display();
+        System.out.println("remove: " + queue.remove());
+        queue.display();
+
+    }
+
+    private static void testDeue() {
+        Deque<Integer> deque = new DequeImpl<>(5);
+        System.out.println("add element: " + deque.insertRight(2));
+        System.out.println("add element: " + deque.insertLeft(34));
+        System.out.println("add element: " + deque.insertRight(1));
+        System.out.println("add element: " + deque.insertLeft(25));
+        System.out.println("add element: " + deque.insertLeft(3));
+        System.out.println("add element: " + deque.insertLeft(77));
+
+        deque.display();
+        System.out.println("remove: " + deque.removeRight());
+        deque.display();
+        System.out.println("remove: " + deque.removeLeft());
+        deque.display();
+
+    }
+
+
 }
--- GB_Algorithms&DataStructure/src/gb/algorithms/collections/deque/DequeImpl.java
+++ GB_Algorithms&DataStructure/src/gb/algorithms/collections/deque/DequeImpl.java
@@ -1,25 +1,86 @@
 package gb.algorithms.collections.deque;
 
-public class DequeImpl implements Deque{
+public class DequeImpl<E> implements Deque<E>{
+    protected final E[] data;
+    private int size;
+    private int head;
+    private int tail;
+
+    private final int HEAD_DEFAULT = 1;
+    private final int TAIL_DEFAULT = -1;
+
+    public DequeImpl(int maxSize) {
+        this.data = (E[])new Object[maxSize];
+        head = HEAD_DEFAULT;
+        tail = TAIL_DEFAULT;
+    }
     @Override
-    public boolean insertLeft(Object value) {
-        return false;
+    public boolean insertLeft(E value) {
+        if (isFull()) {
+            return false;
+        }
+
+        if(head == 0){
+            head = data.length;
+        }
+
+        data[--head] = value;
+        size++;
+        if(size == 1) tail = head;
+        return true;
     }
 
     @Override
-    public boolean insertRight(Object value) {
-        return false;
+    public boolean insertRight(E value) {
+        if (isFull()) {
+            return false;
+        }
+
+        if (tail == data.length - 1) {
+            tail = TAIL_DEFAULT;
+        }
+
+        data[++tail] = value;
+        size++;
+        if(size == 1) head = tail;
+        return true;
     }
 
     @Override
-    public Object removeLeft() {
-        return null;
+    public E removeLeft() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        E value = data[head];
+        data[head] = null;
+        if(head == data.length - 1) {
+            head = 0;
+        } else {
+            head++;
+        }
+
+        size--;
+        return value;
     }
 
     @Override
-    public Object removeRight() {
-        return null;
+    public E removeRight() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        E value = data[tail];
+        data[tail] = null;
+        if(tail == 0){
+            tail = data.length - 1;
+        }else{
+            tail--;
+        }
+        size--;
+        return value;
     }
+
 
     @Override
     public boolean insert(Object value) {
@@ -32,27 +93,41 @@ public class DequeImpl implements Deque{
     }
 
     @Override
-    public Object peekFront() {
-        return null;
+    public E peekFront() {
+        return data[head];
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean isFull() {
-        return false;
+        return size == data.length;
     }
 
     @Override
     public void display() {
-
+        System.out.println(this);
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < data.length; i++) {
+            sb.append(data[i]);
+            if (i != data.length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
 }
