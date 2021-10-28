ca52f60d7d7f4e02135e50e197634204619ab550
Kate <frauaja@yandex.ru>
2021-10-28 21:05:59 +0300

    lesson7

---
+++ GB_Algorithms&DataStructure/src/gb/algorithms/graph/Graph.java
@@ -0,0 +1,22 @@
+package gb.algorithms.graph;
+
+public interface Graph {
+    void addVertex(String label);
+
+    boolean addEdge(String startLabel, String secondLabel, String... others);
+    boolean addEdge(String startLabel, String secondLabel);
+
+    int getSize();
+
+    void display();
+
+    /**
+     * англ. Depth-first search, DFS
+     */
+    void dfs(String startLabel);
+
+    /**
+     * англ. breadth-first search, BFS
+     */
+    void bfs(String startLabel);
+}
