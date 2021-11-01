fcc7584e25aa6a7a470e688ba98921a0633113d4
Kate <frauaja@yandex.ru>
2021-11-01 18:04:55 +0300

    Lesson8

--- GB_Algorithms&DataStructure/src/gb/algorithms/MainTest8.java
+++ GB_Algorithms&DataStructure/src/gb/algorithms/MainTest8.java
@@ -1,7 +1,29 @@
 package gb.algorithms;
 
+import gb.algorithms.hashtable.Product;
+import gb.algorithms.hashtable.homework.HashTableImpl;
+
 public class MainTest8 {
     public static void main(String[] args) {
+        var hashTable = new HashTableImpl<>(5);
 
+        hashTable.put(new Product(1, "Orange"), 150);
+        hashTable.put(new Product(77, "Banana"), 100);
+        hashTable.put(new Product(60, "Lemon"), 250);
+        hashTable.put(new Product(1, "Orange"), 20);
+        hashTable.put(new Product(61, "Cookie"), 120);
+
+        System.out.println("Size is " + hashTable.size());
+        hashTable.display();
+
+
+        System.out.println("Cost banana is " + hashTable.get(new Product(77, "Banana")));
+        System.out.println("Cost carrot is " + hashTable.get(new Product(67, "Carrot")));
+        System.out.println("Cost orange is " + hashTable.get(new Product(1, "Orange")));
+
+        System.out.println(hashTable.remove(new Product(1, "Orange")));
+
+        System.out.println("Size is " + hashTable.size());
+        hashTable.display();
     }
 }
--- GB_Algorithms&DataStructure/src/gb/algorithms/hashtable/homework/HashTableImpl.java
+++ GB_Algorithms&DataStructure/src/gb/algorithms/hashtable/homework/HashTableImpl.java
@@ -48,41 +48,128 @@ public class HashTableImpl<K, V> implements HashTable<K, V> {
     }
 
     public HashTableImpl() {
+
         this(16);
     }
 
 
     private int hashFunc(K key) {
+
         return Math.abs(key.hashCode()) % data.length;
     }
 
     @Override
     public boolean put(K key, V value) {
-        return false;
+        int index = hashFunc(key);
+
+
+        if (data[index] == null) {
+            data[index] = new Item<>(key, value, null);
+            size++;
+            return true;
+        }
+        Item<K,V> element = data[index];
+        do{
+            if(isKeysEquals(element, key)){
+                element.setValue(value);
+                return true;
+            }
+        }while (element.next != null);
+
+        data[index] = new Item<>(key, value, element);
+        size++;
+        return true;
     }
+
+    private boolean isKeysEquals(Item<K, V> item, K key) {
+        if (item == emptyEntry) {
+            return false;
+        }
+        return item.getKey() == null ? key == null : item.getKey().equals(key);
+    }
+
 
     @Override
     public V get(K key) {
+
+        int index = indexOf(key);
+        if (index == -1) {
+            return null;
+        }
+
+        Item<K,V> element;
+        element = data[index];
+        while (element != null){
+            if(isKeysEquals(element,key)){
+                return element.getValue();
+            }
+            element = element.next;
+        }
+
         return null;
+    }
+
+    private int indexOf(K key) {
+        int index = hashFunc(key);
+
+        return  index;
     }
 
     @Override
     public V remove(K key) {
-        return null;
+        int index = indexOf(key);
+        if (index == -1) {
+            return null;
+        }
+
+        Item<K,V> element;
+        Item<K,V> elementPrev = null;
+
+        element = data[index];
+        while (!isKeysEquals(element,key) && element != null){
+            elementPrev = element;
+            element = element.next;
+        }
+
+        if (element == null ){
+            return null;
+        }
+
+        if( elementPrev == null){
+            data[index] = element.next;
+            element.next = emptyEntry;
+            size--;
+        } else{
+            elementPrev.next = element.next;
+            element.next = emptyEntry;
+        }
+
+
+
+        return element.getValue();
     }
 
     @Override
     public int size() {
-        return 0;
+
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size != 0;
     }
 
     @Override
     public void display() {
+        Item<K,V> element;
+        for (int i = 0; i < data.length; i++) {
 
+            element = data[i];
+            while (element != null){
+                System.out.printf("%d = [%s]%n", i, element);
+                element = element.next;
+            }
+        }
     }
 }
