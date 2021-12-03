caa7f2bff10befb8cc2b391522d2c0842d729f1c
ticdollyrow <frauaja@yandex.ru>
2021-12-03 09:16:02 +0300

    Lesson3

--- JavaLevel2/src/main/java/Lesson4 JavaFX/src/main/java/com/example/lesson4/Client.java
+++ JavaLevel2/src/main/java/Lesson4 JavaFX/src/main/java/com/example/lesson4/Client.java
@@ -1,14 +1,12 @@
 package com.example.lesson4;
 
 import com.example.lesson4.database.UserData;
-import javafx.collections.ObservableList;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.List;
+import java.io.RandomAccessFile;
 
 public class Client {
     final static int PORT = 8190;
@@ -17,13 +15,16 @@ public class Client {
     private DataOutputStream out;
     private DataInputStream in;
     private Controller controller;
+    private BufferedWriter historyWriter;
 
     public Client(Controller controller) {
         this.controller = controller;
+
         openConnection();
     }
 
     private void openConnection(){
+
         try {
             socket = new Socket("localhost", PORT);
             in = new DataInputStream(socket.getInputStream());
@@ -32,38 +33,31 @@ public class Client {
 
             new  Thread(() -> {
                     try {
-//                        while (true){
-//                           String msgAuth =  in.readUTF();
-//                           if(msgAuth.startsWith(Commands.AUTH_OK.getCommand())){
-//                               String[] split = msgAuth.split(" ");
-//                               controller.addMessage("успешная авторизация под ником " + split[1]);
-//                               controller.setAuth(true);
-//                               break;
-//                           }
-//                        }
 
                         while (true) {
                             String s = in.readUTF();
 
-//                            String msgAuth =  in.readUTF();
                             String msgAuth = s;
                             if(msgAuth.startsWith(Commands.AUTH_OK.getCommand())){
                                 String[] split = msgAuth.split(" ");
-                                controller.addMessage("успешная авторизация под ником " + split[1]);
+                                final String nick = split[1];
+
+                                readHistory(nick);
+                                controller.addMessage("успешная авторизация под ником " + nick);
                                 controller.setAuth(true);
+
+
+                                openWriter(nick);
                                 continue;
                             }
 
                             if(Commands.END_CHAT.getCommand().equals(s)){
-
-                                //controller.setAuth(false);
                                 break;
                             }
 
                             if(Commands.LOG_OUT.getCommand().equals(s)){
-
                                 controller.setAuth(false);
-//                                break;
+                                closeWriter();
                                 continue;
                             }
 
@@ -75,23 +69,27 @@ public class Client {
                             }
 
                             controller.addMessage(s);
+                            System.out.println(s);
+                            historyWriter.write(s + "\n");
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }finally {
-                        closeConnection();
+                       closeConnection();
                     }
 
                 }).start();
 
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+
         }
-
-
     }
 
     private void closeConnection() {
+        closeWriter();
+
         if(socket != null){
             try {
                 socket.close();
@@ -114,6 +112,50 @@ public class Client {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void openWriter(String nick){
+        String fileName = "HISTORY_" + nick + ".txt";
+        try {
+            historyWriter = new BufferedWriter(new FileWriter(fileName, true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void closeWriter(){
+        if(historyWriter != null){
+            try {
+                historyWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void readHistory(String nick){
+        String fileName = "HISTORY_" + nick + ".txt";
+        int countLines = 0;
+        StringBuilder builder = new StringBuilder();
+        try (RandomAccessFile raf = new RandomAccessFile(fileName, "r")) {
+
+            long length = raf.length() - 1;
+            System.out.println(length);
+            while (length > 0 && countLines < 100) {
+                raf.seek(length);
+                char c = (char) raf.read();
+                System.out.println(c);
+                if(c == '\n'){
+                    countLines++;
+                }
+                builder.append(c);
+                length--;
+            }
+
+            builder.reverse();
+            controller.addMessage(builder.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
