784ffce8c08c9f30f17e12fe21bf21ddccaec5c7
ticdollyrow <frauaja@yandex.ru>
2021-12-12 11:09:29 +0300

    Lesson4

--- Thread/TestApp/src/main/java/TestThread.java
+++ Thread/TestApp/src/main/java/TestThread.java
@@ -1,64 +1,19 @@
-import java.util.SortedMap;
-
-public class TestThread {
+public class TestThread implements  Runnable{
 	private final static int SIZE = 5;
 	private final Object mon = new Object();
 	private volatile char currentLetter = 'A';
-	
+
 	public void runThread(){
-
 		Thread threadA = new Thread(() -> {
-			synchronized (mon) {
-				try {
-				for (int i = 0; i < SIZE; i++) {
-						while (currentLetter != 'A') {
-							mon.wait();
-						}
-						System.out.print("A");
-						currentLetter = 'B';
-						mon.notifyAll();
-					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-
+				printLetter('A', 'B');
 		});
 
 		Thread threadB = new Thread(() -> {
-			synchronized (mon) {
-				try {
-					for (int i = 0; i < SIZE; i++) {
-						while (currentLetter != 'B') {
-							mon.wait();
-						}
-						System.out.print("B");
-						currentLetter = 'C';
-						mon.notifyAll();
-					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-
+				printLetter('B', 'C');
 		});
 
 		Thread threadC = new Thread(() -> {
-			synchronized (mon) {
-				try {
-					for (int i = 0; i < SIZE; i++) {
-						while (currentLetter != 'C') {
-							mon.wait();
-						}
-						System.out.print("C");
-						currentLetter = 'A';
-						mon.notifyAll();
-					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-
+				printLetter('C', 'A');
 		});
 
 		threadA.start();
@@ -75,6 +30,28 @@ public class TestThread {
 
 		System.out.println();
 		System.out.println("END");
+
+	}
+
+	public void printLetter(char letterFirst, char letterNext){
+		synchronized (mon) {
+			try {
+				for (int i = 0; i < SIZE; i++) {
+					while (currentLetter != letterFirst) {
+						mon.wait();
+					}
+					System.out.print(letterFirst);
+					currentLetter = letterNext;
+					mon.notifyAll();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void run() {
 
 	}
 }
